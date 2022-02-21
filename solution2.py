85659e9a899ca053da83f8b779d829e9c0010282
Malso86 <adam.klimko1@gmail.com>
2022-02-21 19:01:19 +0100

    refactor: update solution2, change solution1 to solution3

--- solution2.py
+++ solution2.py
@@ -11,23 +11,32 @@ class Shared:
 
 def test(shared, mutex):
     while 1:
+        # thread acquires lock
         mutex.lock()
         if shared.counter >= shared.end:
+            # first thread to reach array end frees lock and breaks out of while loop
             mutex.unlock()
             break
-        mutex.unlock()
         shared.elms[shared.counter] += 1
         shared.counter += 1
+        # thread frees lock
+        mutex.unlock()
 
 
-mutex = Mutex()
+# test multiple times since parallel programming problems occur randomly
+def try_ten_times():
+    for i in range(10):
+        mutex = Mutex()
+        shared = Shared(1_000_000)
 
-shared = Shared(1_000_000)
-thread1 = Thread(test, shared, mutex)
-thread2 = Thread(test, shared, mutex)
+        thread1 = Thread(test, shared, mutex)
+        thread2 = Thread(test, shared, mutex)
 
-thread1.join()
-thread2.join()
+        thread1.join()
+        thread2.join()
 
-counter = Counter(shared.elms)
-print(counter.most_common())
+        counter = Counter(shared.elms)
+        print(counter.most_common())
+
+
+try_ten_times()
--- solution3.py
+++ solution3.py
@@ -17,20 +17,26 @@ def test(shared, mutex):
             # first thread to reach array end frees lock and breaks out of while loop
             mutex.unlock()
             break
-        shared.elms[shared.counter] += 1
-        shared.counter += 1
         # thread frees lock
         mutex.unlock()
+        shared.elms[shared.counter] += 1
+        shared.counter += 1
 
 
-mutex = Mutex()
+# test multiple times since parallel programming problems occur randomly
+def try_ten_times():
+    for i in range(10):
+        mutex = Mutex()
+        shared = Shared(1_000_000)
 
-shared = Shared(100_000_000)
-thread1 = Thread(test, shared, mutex)
-thread2 = Thread(test, shared, mutex)
+        thread1 = Thread(test, shared, mutex)
+        thread2 = Thread(test, shared, mutex)
 
-thread1.join()
-thread2.join()
+        thread1.join()
+        thread2.join()
 
-counter = Counter(shared.elms)
-print(counter.most_common())
+        counter = Counter(shared.elms)
+        print(counter.most_common())
+
+
+try_ten_times()

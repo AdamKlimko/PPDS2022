1e359f571eba99da2e7b8ff839e7e9f13d5b51e8
Adam Klimko <adam.klimko@gmail.com>
2022-03-14 18:57:36 +0100

    feat: add class Lightswitch

--- main.py
+++ main.py
@@ -5,14 +5,37 @@ description
 """
 
 # Generic/Built-in
+from random import randint
+from time import sleep
 
 # Other Libs
+from fei.ppds import Mutex, Semaphore, Thread
 
 __author__ = "Adam Klimko"
-__copyright__ = "Copyright 2022, Adam Klimko"
-__license__ = "MIT"
 __version__ = "1.0.0"
 __email__ = "xklimko@stuba.sk"
+
+
+class Lightswitch:
+    def __init__(self):
+        self.mutex = Mutex()
+        self.counter = 0
+
+    def lock(self, semaphore):
+        self.mutex.lock()
+        self.counter += 1
+        if self.counter == 1:
+            semaphore.wait()
+        self.mutex.unlock()
+
+    def unlock(self, semaphore):
+        self.mutex.lock()
+        self.counter -= 1
+        if self.counter == 0:
+            semaphore.signal()
+        self.mutex.unlock()
+
+
 
 if __name__ == '__main__':
     pass

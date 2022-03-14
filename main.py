f5bda38e17321d0894347062111ea7dddb829ff5
Adam Klimko <adam.klimko@gmail.com>
2022-03-14 14:26:56 +0100

    docs: prepare main.py file header

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""Exercise 4 of the course PPDS at FEI STU Bratislava
+
+description
+"""
+
+# Generic/Built-in
+
+# Other Libs
+
+__author__ = "Adam Klimko"
+__copyright__ = "Copyright 2022, Adam Klimko"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__email__ = "xklimko@stuba.sk"
+
+if __name__ == '__main__':
+    pass

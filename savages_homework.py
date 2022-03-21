890c74657514aeb646654a275930406c4ab595b0
Adam Klimko <adam.klimko@gmail.com>
2022-03-21 19:27:03 +0100

    feat: add cook threads

---
+++ savages_homework.py
@@ -0,0 +1,82 @@
+from time import sleep
+from random import randint
+from fei.ppds import print, Semaphore, Mutex, Thread
+
+N_SAVAGES = 3
+N_COOKS = 3
+N_SERVINGS = 3
+
+class SimpleBarrier(object):
+    def __init__(self, n_savages):
+        self.n_savages = n_savages
+        self.cnt = 0
+        self.mutex = Mutex()
+        self.barrier = Semaphore(0)
+
+    def wait(self, each = None, last = None):
+        self.mutex.lock()
+        self.cnt += 1
+        if each:
+            print(each)
+        if self.cnt == self.n_savages:
+            if last:
+                print(last)
+            self.cnt = 0
+            self.barrier.signal(self.n_savages)
+        self.mutex.unlock()
+        self.barrier.wait()
+
+
+class Shared(object):
+    def __init__(self, m):
+        self.servings = m
+        self.mutex = Mutex()
+        self.empty_pot = Semaphore(0)
+        self.full_pot = Semaphore(0)
+        self.barrier1 = SimpleBarrier(N_SAVAGES)
+        self.barrier2 = SimpleBarrier(N_SAVAGES)
+
+
+def eat():
+    sleep(randint(50, 200) / 100)
+
+
+def savage(i, shared):
+    sleep(randint(1,100)/100)
+    while True:
+        shared.barrier1.wait()
+        shared.barrier2.wait(each = f'savage {i}: waiting for dinner',
+                             last = f'savage {i}: everybody eating')
+
+        shared.mutex.lock()
+        if shared.servings == 0:
+            print(f'savage {i}: pot empty!')
+            shared.empty_pot.signal()
+            shared.full_pot.wait()
+        print(f'savage {i}: take from pot')
+        shared.servings -= 1
+        shared.mutex.unlock()
+        eat()
+
+
+def cook(i, shared):
+    while True:
+        shared.empty_pot.wait()
+        print(f'cook {i}: cooking')
+        sleep(randint(50, 200) / 100)
+        print(f'cook {i}: put {N_SERVINGS} servings in pot')
+        shared.servings += N_SERVINGS
+        shared.full_pot.signal()
+
+
+def main():
+    shared = Shared(0)
+    savages = [Thread(savage, i, shared) for i in range(N_SAVAGES)]
+    cooks = [Thread(cook, i, shared) for i in range(N_COOKS)]
+
+    for t in savages + cooks:
+        t.join()
+
+
+if __name__ == '__main__':
+    main()

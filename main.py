0cdcd0511b8a4c93ec77ca15d6185c86b3639d83
Malso86 <adam.klimko1@gmail.com>
2022-04-25 17:39:52 +0200

    feat: add main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+from __future__ import division
+from numba import cuda
+import numpy
+import math
+
+
+# Shape of matrix (SIZE x SIZE)
+SIZE = 8
+
+
+@cuda.jit
+def my_kernel_2D(input, output):
+    """
+    Function transposes input matrix and saves the result in output matrix
+    :param input:  Matrix to transpose
+    :param output: Dummy matrix of same size as output
+    """
+    x, y = cuda.grid(2)
+    x_max, y_max = input.shape
+    if x < x_max and y < y_max:
+        output[x, y] = input[y, x]
+
+
+matrix1 = numpy.random.randint(5, size=(SIZE, SIZE))
+matrix2 = numpy.zeros((SIZE, SIZE))
+
+threadsperblock = (SIZE, SIZE)
+blockspergrid_x = math.ceil(matrix1.shape[0] / threadsperblock[0])
+blockspergrid_y = math.ceil(matrix1.shape[1] / threadsperblock[1])
+blockspergrid = (blockspergrid_x, blockspergrid_y)
+
+print(matrix1)
+my_kernel_2D[blockspergrid, threadsperblock](matrix1, matrix2)
+print(matrix2)

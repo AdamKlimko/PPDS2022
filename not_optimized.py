8bb7e74b7ba593cad3a4204661ac98a489ef128e
Malso86 <adam.klimko1@gmail.com>
2022-05-02 16:43:35 +0200

    feat: add not_optimized.py

--- not_optimized.py
+++ not_optimized.py
@@ -3,18 +3,20 @@
 In this exercise we explore the benefits of CUDA programming
 """
 
+from __future__ import division
 __author__ = "Adam Klimko"
 __version__ = "1.0.0"
 __email__ = "xklimko@stuba.sk"
 
-from __future__ import division
+from time import perf_counter
 from numba import cuda
+
 import numpy
 import math
 
 
 # Shape of matrix (SIZE x SIZE)
-SIZE = 8
+SIZE = 200
 
 
 @cuda.jit
@@ -30,14 +32,18 @@ def my_kernel_2D(input, output):
         output[x, y] = input[y, x]
 
 
-matrix1 = numpy.random.randint(5, size=(SIZE, SIZE))
-matrix2 = numpy.zeros((SIZE, SIZE))
+if __name__ == '__main__':
+    matrix1 = numpy.random.randint(5, size=(SIZE, SIZE))
+    matrix2 = numpy.zeros((SIZE, SIZE))
 
-threadsperblock = (SIZE, SIZE)
-blockspergrid_x = math.ceil(matrix1.shape[0] / threadsperblock[0])
-blockspergrid_y = math.ceil(matrix1.shape[1] / threadsperblock[1])
-blockspergrid = (blockspergrid_x, blockspergrid_y)
+    threadsperblock = (SIZE, SIZE)
+    blockspergrid_x = math.ceil(matrix1.shape[0] / threadsperblock[0])
+    blockspergrid_y = math.ceil(matrix1.shape[1] / threadsperblock[1])
+    blockspergrid = (blockspergrid_x, blockspergrid_y)
 
-print(matrix1)
-my_kernel_2D[blockspergrid, threadsperblock](matrix1, matrix2)
-print(matrix2)
+    start_time = perf_counter()
+    print(matrix1)
+    my_kernel_2D[blockspergrid, threadsperblock](matrix1, matrix2)
+    print(matrix2)
+    end_time = perf_counter()
+    print(f'Total time: {round(end_time - start_time, 2)}s')
